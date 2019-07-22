6111afaebe39e237bfbecf6a8d8579b765e15ca7
hanjiyeong <iam.hanjiyeong@gmail.com>
2019-07-22 20:21:12 +0900

    내 소개

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,8 +1,60 @@
 <template>
-  <article role="article" class="wrapper" id="home">
+  <article role="article" class="wrapper padding" id="home">
     <h1 class="screen-out">About</h1>
-    <div class="home-section">
-
+    <div class="inner">
+      <div class="about-overview">
+        <p>웹에이전시에서 근무하다가 현재는 프리랜서로 활동 중이다. 디자인, 퍼블리싱을 병행하여 작업하고 있지만 어느 한 분야만을 지향하지는 않으며, 서비스를 개발하는 데 있어 필요한 업무를 유연하고 다양하게 경험하기를 원한다.</p>
+        <p>기획을 하든, 디자인을 하든, 퍼블리싱을 하든 최종 목표는 결국 좋은 서비스를 만드는 것이라고 생각한다. 좋은 서비스를 만들기 위해서는 늘 내가 알고 있던 것, 내가 잘하는 것 그 이상의 스펙과 그때마다 생기는 내 스스로의 갈증이 있었고, 시간과 체력이 허용되는 한 그런 것들을 해결하고자 이것저것 시도는 참 많이 해봤던 것 같다.</p>
+        <p>십 년 전쯤 어디선가 읽은 한 디자이너의 인터뷰에서 발견한 '옳은가, 쉬운가, 다른가'라는 문구는 보자마자 강렬하고 선명하게 내 마음속에 각인되었고 이후로 나의 모토가 되었다. 이 세 가지 질문은 나에게 매번 다른 형상의 고민거리를 안겨주고 크고 작은 판단의 기로에 수없이 서게 만들었지만 돌이켜보면 이런 생각의 조각들이 쌓여 크진 않지만 적당한 하나의 버팀목이 되어 나를 지탱해준 것 같다. 앞으로 어떤 분야의 일을 하더라도 이 세 가지 질문은 기꺼이 던져볼 것이다.</p>
+      </div>
+      <section class="about-section">
+        <h2 class="section-title">UX/UI</h2>
+        <div class="description">
+          <p></p>
+        </div>
+      </section>
+      <section class="about-section">
+        <h2 class="section-title">Design System</h2>
+        <div class="description">
+          <p></p>
+        </div>
+      </section>
+      <section class="about-section">
+        <h2 class="section-title">Interaction Design</h2>
+        <div class="description">
+          <p></p>
+        </div>
+      </section>
+      <section class="about-section">
+        <h2 class="section-title">Sketch</h2>
+        <div class="description">
+          <p></p>
+        </div>
+      </section>
+      <section class="about-section">
+        <h2 class="section-title">SASS</h2>
+        <div class="description">
+          <p></p>
+        </div>
+      </section>
+      <section class="about-section">
+        <h2 class="section-title">Javascript</h2>
+        <div class="description">
+          <p></p>
+        </div>
+      </section>
+      <section class="about-section">
+        <h2 class="section-title">vue.js</h2>
+        <div class="description">
+          <p></p>
+        </div>
+      </section>
+      <section class="about-section">
+        <h2 class="section-title">Writing</h2>
+        <div class="description">
+          <p></p>
+        </div>
+      </section>
     </div>
   </article>
 </template>
